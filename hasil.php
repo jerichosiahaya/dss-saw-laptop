26750484725d29580bac66ef5ca57beb5fc80a6b
Ricky Ng <56551192+rickyreplying@users.noreply.github.com>
2021-04-28 12:59:21 +0700

    Add files via upload

--- hasil.php
+++ hasil.php
@@ -24,71 +24,98 @@ $jumlah_kriteria = mysqli_num_rows($query_kriteria);
 </head>
 
 <body>
-    <h1>Daftar Alternatif dan Kriterianya </h1>
-    <table>
-        <tr>
-            <th rowspan="2">Alternatif Laptop</th>
-            <th colspan="<?php echo $jumlah_kriteria; ?>">Kriteria</th>
-        <tr>
-            <?php
+
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="https://i.ibb.co/mRgTp8V/laptopp.png" alt="" width="100" height="29">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#">Github</a>
+                </div>
+            </div>
+            <span class="navbar-text">
+                <a href="logout.php">LOGOUT</a>
+            </span>
+        </div>
+    </nav>
+    <!-- navbar -->
+
+    <div class="container mt-4">
+        <h1>Daftar Alternatif dan Kriterianya </h1>
+        <table class="table mt-4">
+            <tr>
+                <th rowspan="2">Alternatif Laptop</th>
+                   <!--<th colspan="<?php echo $jumlah_kriteria; ?>"></th>-->
+            <tr>
+                <?php
                 $kriteria = mysqli_query($conn, "SELECT * FROM `kriteria`") or die(mysqli_error());
                 while($data = mysqli_fetch_array($kriteria)) {
                 ?>
-            <th>
-                <?php echo $data['nama']; ?></th>
+                <th>
+                    <?php echo $data['nama']; ?></th>
 
-            <?php } ?>
-        </tr>
+                <?php } ?>
+            </tr>
 
-        <?php
+            <?php
             $alternatif = mysqli_query($conn, "SELECT * FROM alternatif WHERE id_pengguna = $id");
             while ($data = mysqli_fetch_array($alternatif)) {
             ?>
-        <tr>
-            <td>
-                <?php echo $data['nama']; ?>
-            </td>
-            <?php
+            <tr>
+                <td>
+                    <?php echo $data['nama']; ?>
+                </td>
+                <?php
             $id = $data['id_alternatif'];
              $nilai_alternatif = mysqli_query($conn, "select nilai_alternatif.*, nilai_kriteria.*from nilai_alternatif join nilai_kriteria on nilai_kriteria.id_nilai = nilai_alternatif.id_nilai where  nilai_alternatif.id_alternatif = $id");
             while ($data =  mysqli_fetch_array($nilai_alternatif)) { ?>
-            <td>
-                <?php echo $data['keterangan']; ?>
-            </td>
+                <td>
+                    <?php echo $data['keterangan']; ?>
+                </td>
 
+                <?php } ?>
+            </tr>
             <?php } ?>
-        </tr>
-        <?php } ?>
 
-    </table>
+        </table>
 
 
 
-    <h1>Normalisasi </h1>
-    <table>
-        <tr>
-            <th rowspan="2">Alternatif Laptop</th>
-            <th colspan="<?php echo $jumlah_kriteria; ?>">Kriteria</th>
-        <tr>
-            <?php
+        <h1>Normalisasi </h1>
+        <table class="table mt-4">
+            <tr>
+                <th rowspan="2">Alternatif Laptop</th>
+                    <!--<th colspan="<?php echo $jumlah_kriteria; ?>"></th>-->
+            <tr>
+                <?php
                 $kriteria = mysqli_query($conn, "SELECT * FROM `kriteria`") or die(mysqli_error());
                 while($data = mysqli_fetch_array($kriteria)) {
                 ?>
-            <th>
-                <?php echo $data['nama']; ?></th>
+                <th>
+                    <?php echo $data['nama']; ?></th>
 
-            <?php } ?>
-        </tr>
+                <?php } ?>
+            </tr>
 
-        <?php
+            <?php
             $alternatif = mysqli_query($conn, "SELECT * FROM alternatif WHERE id_pengguna = $id2");
             while ($data = mysqli_fetch_array($alternatif)) {
             ?>
-        <tr>
-            <td>
-                <?php echo $data['nama']; ?>
-            </td>
-            <?php
+            <tr>
+                <td>
+                    <?php echo $data['nama']; ?>
+                </td>
+                <?php
             $id = $data['id_alternatif'];
                 
             $hasil_normalisasi=0;
@@ -102,75 +129,75 @@ $jumlah_kriteria = mysqli_num_rows($query_kriteria);
                     join kriteria on kriteria.id_kriteria = nilai_alternatif.id_kriteria where kriteria.id_kriteria = $id_kriteria");
                 
                     while($data_min = mysqli_fetch_array($min)){ ?>
-            <td>
+                <td>
 
-                <?php
+                    <?php
                      echo number_format($hasil = $data_min['min']/$data['bobot'],3);
                       $hasil_kali = $hasil*$data['weight'];
                       $hasil_normalisasi=$hasil_normalisasi+$hasil_kali;
 
                    ?>
 
-            </td>
-            <?php } ?>
+                </td>
+                <?php } ?>
 
-            <?php }elseif ($data['sifat']=="benefit") {
+                <?php }elseif ($data['sifat']=="benefit") {
                 $id_kriteria = $data['id_kriteria'];
                 $max = mysqli_query($conn, "select kriteria.*, nilai_alternatif.*, nilai_kriteria.id_nilai, nilai_kriteria.id_kriteria, nilai_kriteria.keterangan, MAX(nilai_kriteria.bobot) as max from nilai_alternatif
                 join nilai_kriteria on nilai_kriteria.id_nilai = nilai_alternatif.id_nilai 
                 join kriteria on kriteria.id_kriteria = nilai_alternatif.id_kriteria where kriteria.id_kriteria = $id_kriteria");
             while ($data_max = mysqli_fetch_array($max)) { ?>
-            <td>
+                <td>
 
-                <?php
+                    <?php
                     echo number_format($hasil = $data['bobot']/$data_max['max'],3);
             
 
                   ?>
 
-            </td>
-            <?php   } ?>
-            <?php }
+                </td>
+                <?php   } ?>
+                <?php }
         ?>
 
-            <?php } } ?>
+                <?php } } ?>
 
-        </tr>
-
-
-    </table>
+            </tr>
 
 
-    <h1>Pembobotan </h1>
-    <table>
-        <tr>
-            <th rowspan="2">Alternatif Laptop</th>
-            <th colspan="<?php echo $jumlah_kriteria; ?>">Kriteria</th>
-            <th rowspan="2">Hasil Pembobotan</th>
-        <tr>
-            <?php
+        </table>
+
+
+        <h1>Pembobotan </h1>
+        <table class="table mt-4">
+            <tr>
+                <th rowspan="2">Alternatif Laptop</th>
+                <!--<th colspan="<?php echo $jumlah_kriteria; ?>"></th>-->
+                <th rowspan="2">Hasil Pembobotan</th>
+            <tr>
+                <?php
                 $kriteria = mysqli_query($conn, "SELECT * FROM `kriteria`") or die(mysqli_error());
                 while($data = mysqli_fetch_array($kriteria)) {
                 ?>
-            <th>
-                <?php echo $data['nama']; ?></th>
+                <th>
+                    <?php echo $data['nama']; ?></th>
 
-            <?php } ?>
-        </tr>
+                <?php } ?>
+            </tr>
 
-        <?php
+            <?php
            $hasil_ranks=array();
             $alternatif = mysqli_query($conn, "SELECT * FROM alternatif WHERE id_pengguna = $id2");
             while ($data = mysqli_fetch_array($alternatif)) {
             ?>
-        <tr>
-            <td>
-                <?php echo $data['nama']; 
+            <tr>
+                <td>
+                    <?php echo $data['nama']; 
                 $hasil_rank2['nama'] = $data['nama'];
                 ?>
-                
-            </td>
-            <?php
+
+                </td>
+                <?php
             $id = $data['id_alternatif'];
                 
             $hasil_normalisasi=0;
@@ -184,27 +211,27 @@ $jumlah_kriteria = mysqli_num_rows($query_kriteria);
                     join kriteria on kriteria.id_kriteria = nilai_alternatif.id_kriteria where kriteria.id_kriteria = $id_kriteria");
                 
                     while($data_min = mysqli_fetch_array($min)){ ?>
-            <td>
+                <td>
 
-                <?php
+                    <?php
                       number_format($hasil = $data_min['min']/$data['bobot'],3);
                       echo  number_format($hasil_kali = $hasil*$data['weight'], 3);
                       $hasil_normalisasi=$hasil_normalisasi+$hasil_kali;
 
                    ?>
 
-            </td>
-            <?php } ?>
+                </td>
+                <?php } ?>
 
-            <?php }elseif ($data['sifat']=="benefit") {
+                <?php }elseif ($data['sifat']=="benefit") {
                 $id_kriteria = $data['id_kriteria'];
                 $max = mysqli_query($conn, "select kriteria.*, nilai_alternatif.*, nilai_kriteria.id_nilai, nilai_kriteria.id_kriteria, nilai_kriteria.keterangan, MAX(nilai_kriteria.bobot) as max from nilai_alternatif
                 join nilai_kriteria on nilai_kriteria.id_nilai = nilai_alternatif.id_nilai 
                 join kriteria on kriteria.id_kriteria = nilai_alternatif.id_kriteria where kriteria.id_kriteria = $id_kriteria");
             while ($data_max = mysqli_fetch_array($max)) { ?>
-            <td>
+                <td>
 
-                <?php
+                    <?php
                     number_format($hasil = $data['bobot']/$data_max['max'],3);
                     echo  number_format($hasil_kali = $hasil*$data['weight'], 3);
                     $hasil_normalisasi=$hasil_normalisasi+$hasil_kali;
@@ -214,17 +241,17 @@ $jumlah_kriteria = mysqli_num_rows($query_kriteria);
 
                   ?>
 
-            </td>
+                </td>
 
 
-            <?php   } ?>
-            <?php } ?>
-            <?php 
+                <?php   } ?>
+                <?php } ?>
+                <?php 
             }   
             ?>
 
-            <td>
-                <?php
+                <td>
+                    <?php
                 
              
                 $hasil_rank['nilai'] = $hasil_normalisasi;
@@ -235,34 +262,36 @@ $jumlah_kriteria = mysqli_num_rows($query_kriteria);
            
             
         ?>
-            </td>
-            <?php } ?>
-        </tr>
-    </table>
+                </td>
+                <?php } ?>
+            </tr>
+        </table>
 
-        
-    <table>
-  <tr>
-    <th>Ranking</th>
-    <th>Alternatif Laptop</th>
-    <th>Nilai Akhir</th>
-  </tr>
-  <?php
+
+        <table class="table mt-4">
+            <tr>
+                <th>Ranking</th>
+                <th>Alternatif Laptop</th>
+                <th>Nilai Akhir</th>
+            </tr>
+            <?php
   $no = 1;
   rsort($hasil_ranks);
   foreach ($hasil_ranks as $rank) { ?>
-  <tr>
-    <td><?php echo $no++ ?></></td>
-    <td><?php echo $rank['nama']; ?></td>
-    <td><?php echo $rank['nilai']; ?></td>
-  </tr>
-  <?php } ?>
+            <tr>
+                <td><?php echo $no++ ?></>
+                </td>
+                <td><?php echo $rank['nama']; ?></td>
+                <td><?php echo $rank['nilai']; ?></td>
+            </tr>
+            <?php } ?>
 
 
 
-</table>
+        </table>
 
-    
+
+    </div>
 </body>
 
 </html>
--- register_process.php
+++ register_process.php
@@ -7,9 +7,30 @@ if (empty($username) || empty($password)) {
 } else {
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
     $query = "insert into pengguna (username, password) values ('$username', '$password_hash')";
-    if (mysqli_query($conn, $query)) {
+
+    $query2 = mysqli_query($conn, "SELECT id_pengguna FROM pengguna WHERE username = $username");
+    while ($data = mysqli_fetch_array($query2)) {
+        $id_pengguna = $data['id_pengguna'];
+    }
+    
+    $query3 = "insert into kriteria(id_kriteria, nama, sifat, weight, id_pengguna) VALUES
+    ('1, 'Harga', 'cost', 0.2, $id_pengguna),
+    ('2, 'Processor', 'benefit', 0.15, $id_pengguna),
+    ('3, 'RAM', 'benefit', 0.15, $id_pengguna),
+    ('4, 'Storage', 'benefit', 0.15, $id_pengguna),
+    ('5, 'Battery Life', 'benefit', 0.1, $id_pengguna),
+    ('6, 'VGA Card', 'benefit', 0.15, $id_pengguna),
+    ('7, 'Layar', 'benefit', 0.1, $id_pengguna);";
+
+
+
+
+    if (mysqli_query($conn, $query,$query2, $query3)) {
         header('Location: login.php');
     } else {
         header('Location: register.php');
     }
+
 }
+
+?>
