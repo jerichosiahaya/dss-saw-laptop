040c2031c7e4d2ea0ee4700a46df2b09e4b2a245
Jericho Siahaya <jericho.siahaya@gmail.com>
2021-04-30 01:24:09 +0900

    1:24AM code

--- update_nama_alternatif_proses.php
+++ update_nama_alternatif_proses.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 	require_once 'config.php';
 	require_once 'session.php';
@@ -12,19 +11,3 @@
 
 		header("location: index.php");
 	}
-=======
-<?php
-	require_once 'config.php';
-	require_once 'session.php';
-	
-	if(ISSET($_POST['update'])){
-		$id2 = $_POST['id_alternatif'];
-		$nama = $_POST['nama'];
-	
-		
-		mysqli_query($conn, "UPDATE `alternatif` SET `nama` = '$nama' WHERE `id_alternatif` = '$id2' AND `id_pengguna` = '$id'") or die(mysqli_error());
-
-		header("location: index.php");
-	}
->>>>>>> Stashed changes
-?>
